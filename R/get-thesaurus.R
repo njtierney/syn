665e8b2ceba31b33eeb4b3fd9325d9e176835bbe
Nicholas Tierney <nicholas.tierney@gmail.com>
2018-11-23 10:01:08 +1100

    adds get_thesaurus to download the words.txt file from github.com/words/moby

---
+++ R/get-thesaurus.R
@@ -0,0 +1,21 @@
+#' Install the thesaurus from moby website
+#'
+#' Installs the thesaurus from the [moby website here](https://github.com/words/moby/blob/master/words.txt)
+#'
+#' @return nothing - it installs the text file
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'  get_thesaurus()
+#' }
+get_thesaurus <- function(){
+
+  if (fs::file_exists(here::here("data-raw/words.txt"))) {
+    stop("File 'words.txt' already exists")
+  }
+  download.file(
+    url = "https://raw.githubusercontent.com/words/moby/master/words.txt",
+    destfile = here::here("data-raw/words.txt")
+  )
+}
