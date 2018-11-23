2024490a449d57c675f9a4ecbb728e2ca1338687
Nicholas Tierney <nicholas.tierney@gmail.com>
2018-11-23 15:34:05 +1100

    export syns

--- R/syn.R
+++ R/syn.R
@@ -1,5 +1,3 @@
-
-
 #' Syn
 #'
 #' @param word word
@@ -13,4 +11,50 @@ syn <- function(word) {
   # Then we can look up our word.
   # What happens when word doesn't exist?
   words_syn[[word]]
+}
+#' Syn: Get synonyms for one word
+#'
+#' @param word character, one word you want to find synonyms for
+#' @param n_words integer, the number of words to look for. An integer Default is all words
+#'
+#' @return synonyms of the word you entered
+#'
+#' @examples
+#' # put some examples here
+#' @export
+syn <- function(word, n_words = -1) {
+  # We have to get our words_syn back out of the environment we stored it in
+  words_syn <- get('words_syn', envir = getOption('syn_env'))
+
+  # Then we can look up our word.
+  # What happens when word doesn't exist?
+  if (n_words < 0) {
+    words_syn[[word]]
+  } else {
+    sample(words_syn[[word]], n_words)
+  }
+
+}
+
+#' Syns: Get synonyms for many words
+#'
+#' @param words word, character - many words you want to find synonyms for
+#' @param n_words integer, the number of words to look for. Default is all words
+#'
+#' @return named list of synonyms
+#' @examples
+#' # put some examples here
+#' @export
+syns <- function(words, n_words = -1) {
+
+  # We have to get our words_syn back out of the environment we stored it in
+  words_syn <- get('words_syn', envir = getOption('syn_env'))
+
+  # Then we can look up our word.
+  # What happens when word doesn't exist?
+  syns <- words_syn[words]
+
+  purrr::map(syns, sample, n_words) %>%
+    purrr::set_names(words)
+
 }
