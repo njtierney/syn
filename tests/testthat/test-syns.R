84d5dc66693b9d731ef83a80a8070322e7063312
Nicholas Tierney <nicholas.tierney@gmail.com>
2018-11-27 14:59:04 +1100

    adds tests for syn and syns

---
+++ tests/testthat/test-syns.R
@@ -0,0 +1,24 @@
+context("test-syns")
+
+test_that("syns pulls the right number of words for cool", {
+  expect_length(syns("cool"), 1)
+  expect_length(syns("cool")[[1]], 618)
+})
+
+test_that("syns pulls the right number of words for cool with n_words", {
+  expect_length(syns("cool", 1), 1)
+  expect_length(syns("cool", 1)[[1]], 1)
+  expect_length(syns("cool", 5)[[1]], 5)
+  expect_length(syns("cool", 10)[[1]], 10)
+})
+
+test_that("syns works with two words", {
+  expect_length(syns(c("cool", "evil")), 2)
+})
+
+test_that("syns n_words works", {
+  expect_length(syns(c("cool", "evil"), 5), 2)
+  expect_length(syns(c("cool", "evil"), 5)[[1]], 5)
+  expect_length(syns(c("cool", "evil"), 5)[[2]], 5)
+})
+
